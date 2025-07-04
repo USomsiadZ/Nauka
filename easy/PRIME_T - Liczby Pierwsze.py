89fb54f086ad91efe2bd374888a474333307aea9
Hubert <hubertparzych10@gmail.com>
2025-07-04 21:47:05 +0200

    Liczby pierwsze

---
+++ easy/PRIME_T - Liczby Pierwsze.py
@@ -0,0 +1,15 @@
+#https://pl.spoj.com/problems/PRIME_T/
+def prime_numer(number: int) -> str:
+    if number < 2:
+        return "NIE"
+    x = 2
+    while x*x <= number:
+        if number % x == 0:
+            return "NIE"
+        x += 1
+    return "TAK"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(prime_numer(n))
